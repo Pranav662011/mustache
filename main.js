d90fca064a24d58cd3c3a7c065da7752ab66c7b5
Pranav662011 <79525414+Pranav662011@users.noreply.github.com>
2021-09-07 19:08:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    
+
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save("MyFilterImage.png");
+}
